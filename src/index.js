21065d700e731884cdbf853ecddc47d474ebb7ac
anny_more <anxxxen@ya.ru>
2022-06-22 09:35:21 +0300

    feeat: fix class KeyBoard

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ export const keyBoard = new KeyBoard;
 
 keyBoard.create();
 
-keyBoard.node.addEventListener('click', (event) => {
-    keyBoard.mouseEventHandler(event);
-});
-
 document.addEventListener('keydown', (event) => {
     keyBoard.keyEventHandler(event);
 });
